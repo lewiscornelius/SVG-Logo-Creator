9660ab75857519ceca1232a951cbbb77c30743cb
lewiscornelius <lewiscornelius7@gmail.com>
2024-02-13 16:33:39 -0500

    test.js

--- library/svg.test.js
+++ library/svg.test.js
@@ -23,3 +23,20 @@ test("should set text attribute and style", () => {
   svg.setText("SVG", "#333");
   expect(svg.render()).toEqual(expectedSvg);
 });
+
+test("should throw if text exceeds 3 characters", () => {
+    const expectedError = new Error("Text must not exceed 3 characters.");
+    const svg = new SVG();
+    expect(() => svg.setText("HELLO", "#333")).toThrow(expectedError);
+  });
+  
+  test("should include a shape", () => {
+    const expectedSvg =
+      '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect x="90" y="40" width="120" height="120" fill="dodgerblue" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="#333">SVG</text></svg>';
+    const svg = new SVG();
+    svg.setText("SVG", "#333");
+    const square = new Square();
+    square.setColor("dodgerblue");
+    svg.setShape(square);
+    expect(svg.render()).toEqual(expectedSvg);
+  });
