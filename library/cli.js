3505781fc14574232cd1520a6b879486bbd6be80
lewiscornelius <lewiscornelius7@gmail.com>
2024-02-13 16:14:07 -0500

    Command Line Interface JavaScript

---
+++ library/cli.js
@@ -0,0 +1,68 @@
+const inquirer = require("inquirer");
+const SVG = require("./svg");
+const { Circle, Triangle, Square } = require("./shapes");
+const { writeFile } = require("fs/promises");
+
+class CLI {
+    run() {
+        return inquirer
+        .createPromptModule([
+            {
+                name: "text",
+                type: "input",
+                message:
+                "Enter the text you want to display (leave empty to display a random text)",
+                validate: (text) =>
+                text.length <= 3 ||
+                "The message must not have more than 3 characters",
+            },
+            {
+                name: "textColor",
+                type: "input",
+                message: "Enter a text color",
+            },
+            {
+                name: "shapeType",
+                type: "list",
+                choices: ["Circle", "Triangle", "Square"],
+                message: "Choose a shape",
+            },
+            {
+                name: "shapeColor",
+                type: "input",
+                message: "Enter a color for your shape:"
+            },
+        ])
+        .then(({ text, textColor, shapeType, shapeColor }) => {
+            let shape;
+            switch (shapeType) {
+                case "circle":
+                    shape = new Circle();
+                    break;
+
+                case "square":
+                    shape = new Square();
+                    break;
+                
+                default:
+                    shape = new Triangle();
+                    break;
+            }
+            shape.setColor(shapeColor);
+
+            const svg = new SVG();
+            svg.setText(text, textColor);
+            svg.setShape(shape);
+            return writeFile("loho.svg", svg.render());
+        })
+        .then(() => {
+            console.log("Generated logo.svg");
+        })
+        .catch((error) => {
+            console.log(error);
+            console.log("Oops! Something went wrong.");
+          });
+    }
+}
+
+module.exports = CLI;
---
+++ library/svg.test.js
@@ -0,0 +1,25 @@
+const SVG = require("./svg");
+const { Square } = require("./shapes");
+
+Test("should render a 300 x 200 svg element", () => {
+    const expectedSvg =
+    '';
+    const svg = new SVG();
+    expect(svg.render()).toEqual(expectedSvg);
+});
+
+test("should append text element", () => {
+    const expectedSvg =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">A</text></svg>';
+  const svg = new SVG();
+  svg.setText("A", "white");
+  expect(svg.render()).toEqual(expectedSvg);
+});
+
+test("should set text attribute and style", () => {
+    const expectedSvg =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><text x="150" y="125" font-size="60" text-anchor="middle" fill="#333">SVG</text></svg>';
+  const svg = new SVG();
+  svg.setText("SVG", "#333");
+  expect(svg.render()).toEqual(expectedSvg);
+});
